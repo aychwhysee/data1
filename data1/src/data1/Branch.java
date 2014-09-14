21d70fa839a702504734650e89acafd56da07d4f
Harry Cho <hacho@vassar.edu>
2014-09-14 16:57:13 -0400

    Added comments and made some changes
    How does one do empty with no arguments?

--- data1/src/data1/Branch.java
+++ data1/src/data1/Branch.java
@@ -1,5 +1,7 @@
 package data1;
 
+
+
 public class Branch implements FiniteSet {
     int root;
     FiniteSet left;
@@ -13,14 +15,20 @@ public class Branch implements FiniteSet {
     }
     
     public int cardinality() {
+        // Counting! Count the stuff on the left and the stuff on the right
+        // then add one for the root.
        return left.cardinality() + right.cardinality() + 1;
     }
     
     public boolean isEmptyHuh() {
+        // Branch is never empty, return false.
         return false;
     }
     
     public boolean member(int elt) {
+        // Check if elt is = to root. If it is, return true. If it's not, check
+        // if it's smaller or greater than root, and check the respective sides
+        // of the branch.
         if (root == elt) {
             return true;
         } else if (elt < root) {
@@ -31,6 +39,8 @@ public class Branch implements FiniteSet {
     }
     
     public FiniteSet add(int elt) {
+        // If elt is already in the branch, just return the branch as it is.
+        // If not, find where it should go and add it there.
         if (root == elt) {
            return this;
         } else if (elt < root) {
@@ -41,6 +51,10 @@ public class Branch implements FiniteSet {
     }
     
     public FiniteSet remove(int elt) {
+        // If elt is = root, then get rid of it by only returning the union of
+        // left and right.
+        // If not, check the respective sides of the branch depending on if elt
+        // is lesser or greater than root.
         if (root == elt) {
             return left.union(right);
         } else if (elt < root) {
@@ -51,11 +65,15 @@ public class Branch implements FiniteSet {
     }
     
     public FiniteSet union(FiniteSet u) {
+        // Add method helps to get rid of duplicates in our set.
         return left.union(right.union(u)).add(root);
     }
     
     public FiniteSet inter(FiniteSet u) {
-        if (u.member(this.root)) {
+        // If root is a member of FiniteSet u, then we put root into our
+        // intersection set and basically do that for the rest of the elements
+        // in our set. Then we return our new set altogether.
+        if (u.member(root)) {
             return new Branch(left.inter(u), root, right.inter(u));
         } else {
             return left.inter(u).union(right.inter(u));
@@ -63,22 +81,29 @@ public class Branch implements FiniteSet {
     }
     
     public FiniteSet diff(FiniteSet u) {
+        // If root is a member of u, don't include it in our new tree.
         if (u.member(root)) {
-            return left.union(right.union(u.remove(root)));
+            return left.union(right.union(u.remove(root)).diff(u));
         } else {
+            // Check the rest and put it all together in the end (needs testing)
             return right.diff(u).union(left.diff(u));
         }
     }
     
     public boolean equal(FiniteSet u) {
+        // Two sets are equal iff they are subsets of each other.
         return this.subset(u) && u.subset(this);
     }
     
     public boolean subset(FiniteSet u) {
+        // If root is not a member of u, then t cannot be a subset of u.
         if(!u.member(root)) {
             return false;
+            // We know that t is a subset of u if the root is a member of u
+            // and there are no other elements in t.
         } else if ((this.left.isEmptyHuh()) || (this.right.isEmptyHuh())) {
             return true;
+            // Check everything else.
         } else {
             return this.left.union(this.right).subset(u);
         }
--- data1/src/data1/Leaf.java
+++ data1/src/data1/Leaf.java
@@ -15,43 +15,53 @@ public class Leaf implements FiniteSet {
     Leaf() {};
     
     public int cardinality() {
+        // Leaf is always empty, return 0
         return 0;
     }
     
     public boolean isEmptyHuh() {
+        // Leaf is always empty, return true
         return true;
     }
     
     public boolean member(int elt) {
+        // Leaf is always empty (no members), return false
         return false;
     }
 
     public FiniteSet add(int elt) {
+        // Since Leaf is empty, we just need to make a new Branch
+        // with elt as the only element in the set
         return new Branch(this, elt, this);
     }
     
     public FiniteSet remove(int elt) {
+        // Removing from an empty set should just return our original empty set
         return this;
     }
     
     public FiniteSet union(FiniteSet u) {
+        // Since leaf is empty, just return u
         return u;
     }
     
     public FiniteSet inter(FiniteSet u) {
+        // The intersection of an empty set and anything is just the empty set
         return this;
     }
     
     public FiniteSet diff(FiniteSet u) {
+        // The difference between a set U and an empty set is just U
         return u;
     }
     
     public boolean equal(FiniteSet u) {
+        // If u is empty, then it is equal to this. Else false.
         return u.isEmptyHuh();
     }
     
     public boolean subset(FiniteSet u) {
+        // An empty set is always a subset of another set.
         return true;
-        // since an empty set is also a subset of a set?
     }
 }
