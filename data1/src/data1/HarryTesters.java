779c16ee8c6860d130a690e403cd388ed82845be
Harry Cho <hacho@vassar.edu>
2014-09-15 17:15:36 -0400

    New setup for testers, also an image.
    Because drawing an actual tree helps?

--- data1/src/data1/HarryTesters.java
+++ data1/src/data1/HarryTesters.java
@@ -10,13 +10,18 @@ class HarryTesters {
             // Empty set
         FiniteSet mT = empty();
         // Left branches
-        FiniteSet t1 = new Branch(mT, 6, mT);
-        FiniteSet t2 = new Branch(mT, 5, t1);
+        //FiniteSet t1 = new Branch(mT, 6, mT);
+        FiniteSet t1 = empty().add(6);
+        //FiniteSet t2 = new Branch(mT, 5, t1);
+        FiniteSet t2 = t1.add(5);
         // Right branches
-        FiniteSet t3 = new Branch(mT, 8, mT);
-        FiniteSet t4 = new Branch(t3, 9, mT);
+        //FiniteSet t3 = new Branch(mT, 8, mT);
+        FiniteSet t3 = empty().add(8);
+        //FiniteSet t4 = new Branch(t3, 9, mT);
+        FiniteSet t4 = t3.add(9);
         // Combined branch with root = 7
-        FiniteSet t5 = new Branch(t2, 7, t4);
+        //FiniteSet t5 = new Branch(t2, 7, t4);
+        FiniteSet t5 = t2.union(t4).add(7);
         
         // Tests! Lots of tests!
         
